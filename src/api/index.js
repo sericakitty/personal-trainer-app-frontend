0102cc5d563c7d0b04955a08f8b59fc58e68fd8c
sericakitty <63978546+sericakitty@users.noreply.github.com>
2024-04-29 08:55:48 +0300

    add extractDataFromResponse function which returns _embedded object from data if exist

--- src/api/index.js
+++ src/api/index.js
@@ -5,3 +5,11 @@ const baseURL = import.meta.env.VITE_API_URL;
 export const axiosInstance = axios.create({
     baseURL
 });
+
+export const extractDataFromResponse = (response) => {
+  // Assuming the relevant data is nested inside `_embedded`
+  if (response.data && response.data._embedded && response.data._embedded) {
+    return response.data._embedded;
+  }
+  return []; // Return an empty array if the expected structure is not found
+}
