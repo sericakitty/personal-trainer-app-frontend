6c65892355309f349edc47defbde4ea784d9a46e
sericakitty <63978546+sericakitty@users.noreply.github.com>
2024-04-29 09:15:08 +0300

    add /trainings route for TrainingsPage component

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,14 @@
 // react router
 import { Routes, Route } from 'react-router-dom';
 
-import CustomersPage from '@/pages/CustomersPage';
+import CustomersPage from '../pages/CustomersPage';
+import TrainingsPage from '../pages/TrainingsPage';
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<CustomersPage />} />
+            <Route path="/trainings" element={<TrainingsPage />} />
         </Routes>
     )
 }
