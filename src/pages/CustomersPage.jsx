1429cac419c327adaba6a66cda9fbc3c5eba0022
sericakitty <63978546+sericakitty@users.noreply.github.com>
2024-05-05 16:03:36 +0300

    add styles; add dialog form for new customer; add "AddBoxIcon" mui-icon for open dialog; add functionality for adding a new customer; rename functions

--- src/pages/CustomersPage.jsx
+++ src/pages/CustomersPage.jsx
@@ -1,40 +1,61 @@
 import { useEffect, useState } from 'react';
 import { getAllCustomers } from "@/api/customers";
+import { addNewCustomer } from "@/api/customers";
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TablePagination, TextField, IconButton, InputAdornment } from '@material-ui/core';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TablePagination, TextField, IconButton, InputAdornment, Dialog, DialogTitle, DialogContent, Grid } from '@material-ui/core';
 import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AddBoxIcon from '@mui/icons-material/AddBox';
 
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
   },
   searchInput: {
-    marginTop: 'auto',
-    marginLeft: 'auto',
-  },
-  actionCell: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  addTrainingButton: {
-    fontSize: '12px',
-    color: 'blue',
+    marginTop: 'auto !important',
+    marginLeft: 'auto !important',
   },
   tableContainer: {
     verticalAlign: 'top'
   },
-  addCustomerForm: {
-    maxWidth: '300px',
-  },
   sortingHeader: {
     padding: '0px',
-    fontSize: '14px',
-    color: 'black',
+    fontSize: '14px !important',
+    color: 'black !important',
+    '&:focus': { // Remove hover effect
+      outline: 'none',
+      border: 'none',
+    },
+    '&:hover': { // Remove hover effect
+      backgroundColor: 'transparent',
+    },
   },
-
+  diaglogAddCustomerButton: {
+    marginTop: 'auto !important',
+    paddingBottom: '5px !important',
+    paddingLeft: '5px !important',
+  },
+  dialogCloseButton: {
+    color: 'red',
+    marginRight: 'auto',
+    border: '1px solid red !important',
+    borderRadius: '5px !important',
+    marginRight: '5px',
+  },
+  addCustomerButton: {
+    color: 'blue',
+    marginLeft: 'auto',
+    border: '1px solid blue !important',
+    borderRadius: '5px !important',
+  },
+  dialogButtons: {
+    marginTop: '10px',
+  },
+  dialogTextFieldContainer: {
+    marginTop: 12, // Adjust the top margin for spacing between text fields
+  },
 });
 
 const CustomersPage = () => {
@@ -45,6 +66,17 @@ const CustomersPage = () => {
   const [searchInput, setSearch] = useState('');
   const [sortField, setSortField] = useState(null);
   const [sortOrder, setSortOrder] = useState('asc');
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const [newCustomer, setNewCustomer] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    phone: '',
+    streetaddress: '',
+    postcode: '',
+    city: ''
+  });
 
   // Fetch all customers
   useEffect(() => {
@@ -91,43 +123,92 @@ const CustomersPage = () => {
     });
   };
 
+  // Handle adding a new customer
+  const handleAddCustomer = async (event) => {
+    event.preventDefault();
+
+    // Validate new customer fields
+    if (newCustomer.firstname !== '' && newCustomer.lastname !== '' && newCustomer.email !== '' && newCustomer.phone !== '' && newCustomer.streetaddress !== '' && newCustomer.postcode !== '' && newCustomer.city !== '') {
+      return;
+      await addNewCustomer(newCustomer);
+      resetDialog();
+      const data = await getAllCustomers();
+      setCustomers(data);
+    }
+  };
+
+  // Reset dialog fields and errors
+  const resetDialog = () => {
+    setDialogOpen(false);
+    setNewCustomer({
+      firstname: '',
+      lastname: '',
+      email: '',
+      phone: '',
+      streetaddress: '',
+      postcode: '',
+      city: ''
+    });
+  }
+
+
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+  }
+
+  const handleDialogOpen = () => {
+    setDialogOpen(true);
+  }
+
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setNewCustomer(prevState => ({
+      ...prevState,
+      [id]: value
+    }));
+  }
+
+
+
   // Table column headers
-  const columnHeaders = () => {
+  const columnHeadersContent = () => {
     return (
       <TableHead>
         <TableRow>
-          <TableCell onClick={() => handleColumnSort('firstname')}>
-            <IconButton className={classes.sortingHeader}>
+          <TableCell>
+            <IconButton
+              className={classes.sortingHeader} disableRipple onClick={() => handleColumnSort('firstname')}>
               First name{sortField === 'firstname' && sortOrder === 'asc' ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
             </IconButton>
           </TableCell>
-          <TableCell onClick={() => handleColumnSort('lastname')}>
-            <IconButton className={classes.sortingHeader}>
+          <TableCell>
+            <IconButton className={classes.sortingHeader} disableRipple onClick={() => handleColumnSort('lastname')}>
               Last name{sortField === 'lastname' && sortOrder === 'asc' ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
             </IconButton>
           </TableCell>
-          <TableCell onClick={() => handleColumnSort('email')}>
-            <IconButton className={classes.sortingHeader}>
+          <TableCell>
+            <IconButton className={classes.sortingHeader} disableRipple onClick={() => handleColumnSort('email')}>
               Email{sortField === 'email' && sortOrder === 'asc' ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
             </IconButton>
           </TableCell>
-          <TableCell onClick={() => handleColumnSort('phone')}>
-            <IconButton className={classes.sortingHeader}>
+          <TableCell>
+            <IconButton className={classes.sortingHeader} disableRipple onClick={() => handleColumnSort('phone')}>
               Phone{sortField === 'phone' && sortOrder === 'asc' ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
             </IconButton>
           </TableCell>
-          <TableCell onClick={() => handleColumnSort('streetaddress')}>
-            <IconButton className={classes.sortingHeader}>
+          <TableCell>
+            <IconButton className={classes.sortingHeader} disableRipple onClick={() => handleColumnSort('streetaddress')}>
               Street address{sortField === 'streetaddress' && sortOrder === 'asc' ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
             </IconButton>
           </TableCell>
-          <TableCell onClick={() => handleColumnSort('postcode')}>
-            <IconButton className={classes.sortingHeader}>
+          <TableCell>
+            <IconButton className={classes.sortingHeader} disableRipple onClick={() => handleColumnSort('postcode')}>
               Postcode{sortField === 'postcode' && sortOrder === 'asc' ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
             </IconButton>
           </TableCell>
-          <TableCell onClick={() => handleColumnSort('city')}>
-            <IconButton className={classes.sortingHeader}>
+          <TableCell>
+            <IconButton className={classes.sortingHeader} disableRipple onClick={() => handleColumnSort('city')}>
               City{sortField === 'city' && sortOrder === 'asc' ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
             </IconButton>
           </TableCell>
@@ -137,8 +218,8 @@ const CustomersPage = () => {
   }
 
   // Table rows
-  const normalCustomersOutput = () => {
-    return customers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((customer, index) => (
+  const normalCustomersContent = () => {
+    return customers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((customer, index) => ( // Slice the array to display only the current page
       <TableRow key={index}>
         <TableCell>{customer.firstname}</TableCell>
         <TableCell>{customer.lastname}</TableCell>
@@ -152,8 +233,8 @@ const CustomersPage = () => {
   }
 
   // Table rows with search filter
-  const searchCustomersOutput = () => {
-    return customers.filter((customer) => {
+  const searchCustomersContent = () => {
+    return customers.filter((customer) => { // Filter customers based on search input
       return customer.firstname.toLowerCase().includes(searchInput.toLowerCase()) ||
         customer.lastname.toLowerCase().includes(searchInput.toLowerCase()) ||
         customer.email.toLowerCase().includes(searchInput.toLowerCase()) ||
@@ -161,7 +242,7 @@ const CustomersPage = () => {
         customer.streetaddress.toLowerCase().includes(searchInput.toLowerCase()) ||
         customer.postcode.toLowerCase().includes(searchInput.toLowerCase()) ||
         customer.city.toLowerCase().includes(searchInput.toLowerCase());
-    }).map((customer, index) => (
+    }).map((customer, index) => ( // Map through filtered customers
       <TableRow key={index}>
         <TableCell>{customer.firstname}</TableCell>
         <TableCell>{customer.lastname}</TableCell>
@@ -174,11 +255,56 @@ const CustomersPage = () => {
     ))
   }
 
+  // Dialog content
+  const dialogContent = () => {
+    return (
+      <Dialog open={dialogOpen} onClose={handleDialogClose}>
+        <form onSubmit={handleAddCustomer}>
+          <DialogTitle>Add New Customer</DialogTitle>
+          <DialogContent>
+            <Grid container direction="column" spacing={2}>
+              {[{ field: 'firstname', type: 'text' },
+              { field: 'lastname', type: 'text' },
+              { field: 'email', type: 'email' },
+              { field: 'phone', type: 'text' },
+              { field: 'streetaddress', type: 'text' },
+              { field: 'postcode', type: 'text' },
+              { field: 'city', type: 'text' }]
+                .map(({ field, type }) => (
+                  <Grid item key={field} className={classes.dialogTextFieldContainer}>
+                    <TextField
+                      id={field}
+                      label={field.replace(/([A-Z])/g, ' $1').replace(/^\w/, (c) => c.toUpperCase())} // Converts camelCase to words with first letter capitalized
+                      type={type}
+                      required
+                      value={newCustomer[field]}
+                      onChange={handleChange}
+                      fullWidth
+                    />
+                  </Grid>
+                ))
+              }
+            </Grid>
+            <div className={classes.dialogButtons}>
+              <IconButton className={classes.dialogCloseButton} onClick={handleDialogClose}>
+                Close
+              </IconButton>
+              <IconButton className={classes.addCustomerButton} type="submit">
+                Add customer
+              </IconButton>
+            </div>
+          </DialogContent>
+        </form>
+      </Dialog>
+    )
+  }
+
 
   return (
     <Paper>
       <TableContainer className={classes.tableContainer}>
         <div style={{ display: 'flex', padding: '16px' }}>
+
           <h3>Customers</h3>
           <TextField
             className={classes.searchInput}
@@ -201,13 +327,16 @@ const CustomersPage = () => {
                 </InputAdornment>
               ),
             }}
-            />
-            
+          />
+          <IconButton className={classes.diaglogAddCustomerButton} onClick={handleDialogOpen}>
+            <AddBoxIcon />
+          </IconButton>
+          {dialogContent()}
         </div>
         <Table className={classes.table} aria-label="simple table">
-          {columnHeaders()}
+          {columnHeadersContent()}
           <TableBody>
-            {searchInput === '' ? normalCustomersOutput() : searchCustomersOutput()}
+            {searchInput === '' ? normalCustomersContent() : searchCustomersContent()}
           </TableBody>
         </Table>
       </TableContainer>
