475e24f84d6e5a621f6c56ea2e56f33e2964fe7e
sericakitty <63978546+sericakitty@users.noreply.github.com>
2024-05-06 17:38:25 +0300

    Add Route for CalendarPage component

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,14 +1,16 @@
 // react router
 import { Routes, Route } from 'react-router-dom';
 
-import CustomersPage from '../pages/CustomersPage';
-import TrainingsPage from '../pages/TrainingsPage';
+import CustomersPage from '@/pages/CustomersPage';
+import TrainingsPage from '@/pages/TrainingsPage';
+import CalendarPage from '@/pages/CalendarPage';
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<CustomersPage />} />
             <Route path="/trainings" element={<TrainingsPage />} />
+            <Route path="/calendar" element={<CalendarPage />} />
         </Routes>
     )
 }
