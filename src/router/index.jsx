215bf5106b7689f7882a9ea09ddf2aae790b71f6
sericakitty <63978546+sericakitty@users.noreply.github.com>
2024-05-07 22:31:50 +0300

    Add Route for StatisticsPage component

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,9 +1,9 @@
-// react router
 import { Routes, Route } from 'react-router-dom';
 
 import CustomersPage from '@/pages/CustomersPage';
 import TrainingsPage from '@/pages/TrainingsPage';
 import CalendarPage from '@/pages/CalendarPage';
+import StatisticsPage from '@/pages/StatisticsPage';
 
 const AppRouter = () => {
     return (
@@ -11,6 +11,7 @@ const AppRouter = () => {
             <Route path="/" element={<CustomersPage />} />
             <Route path="/trainings" element={<TrainingsPage />} />
             <Route path="/calendar" element={<CalendarPage />} />
+            <Route path="/statistics" element={<StatisticsPage />} />
         </Routes>
     )
 }
